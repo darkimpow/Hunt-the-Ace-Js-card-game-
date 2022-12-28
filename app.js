83c691ae7a56572b8e5358b15d665ebee2bf576e
darkimpow <darkimpowell@gmail.com>
2022-12-28 14:17:22 -0500

    changed elements for car reveal transition

--- app.js
+++ app.js
@@ -34,44 +34,44 @@ const CardBackImg = createElement('img')
 addClassToElement(cardElm, 'card')
 addIdToElement(cardElem, cardItem.id)
 
-//add class to inner card element 
-addClassToElement(cardInnerElem, 'card-inner')
+  //add class to inner card element
+  addClassToElement(cardInnerElem, 'card-inner')
+    
+  //add class to front card element
+  addClassToElement(cardFrontElem, 'card-front')
 
-//add class to front card element
-addClassToElement(cardFrontElm, 'card-front')
+  //add class to back card element
+  addClassToElement(cardBackElem, 'card-back')
 
-//add class to back card element
-addClassToElement(cardBackElm, 'card-back')
+  //add src attribute and appropriate value to img element - back of card
+  addSrcToImageElem(cardBackImg, cardBackImgPath)
 
-//add src attribute & meaningful value to img element- back of card
-addSrcToImageElem(cardBackImg, cardBackImgPath )
+  //add src attribute and appropriate value to img element - front of card
+  addSrcToImageElem(cardFrontImg, cardItem.imagePath)
 
-//add src attribute and meaningful value to img element- front of card
-addSrcToImageElem(cardFrontImg, cardItem.imagePath)
+  //assign class to back image element of back of card
+  addClassToElement(cardBackImg, 'card-img')
+ 
+  //assign class to front image element of front of card
+  addClassToElement(cardFrontImg, 'card-img')
 
-//assign class to back image element of back of card
-addClassToElement(cardBackImg, 'card-img')
+  //add front image element as child element to front card element
+  addChildElement(cardFrontElem, cardFrontImg)
 
-//assign class to front image element of front of card
-addClassToElement(cardFrontImg, 'card-img')
+  //add back image element as child element to back card element
+  addChildElement(cardBackElem, cardBackImg)
 
-//add front image element as child to front card elements 
-addChildElement(cardFrontElm, cardFrontImg)
+  //add front card element as child element to inner card element
+  addChildElement(cardInnerElem, cardFrontElem)
 
-//add back image element as child to back card elements 
-addChildElement(cardBackElm, cardBackImg)
+  //add back card element as child element to inner card element
+  addChildElement(cardInnerElem, cardBackElem)
 
-//add front card element as child element to inner card element 
-addChildElement(cardInnerElem, cardFrontElm)
+  //add inner card element as child element to card element
+  addChildElement(cardElem, cardInnerElem)
 
-//add back card element as child element to inner card element 
-addChildElement(cardInnerElem, cardBackElm)
-
-//add inner card element as child element to card element
-addChildElement(cardElem, cardInnerElem)
-
-//add card element as child as child element to appropiate grid cell
-addCardToGridCell(cardElem)
+  //add card element as child element to appropriate grid cell
+  addCardToGridCell(cardElem)
 } 
 
 function createElement(elemType){
