c4674f6a0b7d5c3f8cc28b9fd85735001159af5a
darkimpow <darkimpowell@gmail.com>
2022-12-30 11:25:20 -0500

    chnaged picture sizes for correct card fit in grid, added css for overavll game play animation with card 3d effect outline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const cardObjectDefinitions = [
     {id:1, imagePath:'/images/card-KingHearts.png'},
     {id:2, imagePath:'/images/card-JackClubs.png'},
     {id:3, imagePath:'/images/card-QueenDiamonds.png'},
-    {id:4, imagePath:'/images/card-AceSpades.png'}
+    {id:4, imagePath:'/images/Ace_of_spades.svg.png'}
 ]
 const aceId = 4
 
